62862fde1671be423c439d9b5d8571c920f19724
kpnemre <kpnemre51@gmail.com>
2021-01-09 18:18:11 +0300

    Add model and form

---
+++ src/todo/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import fields
+from .models import Todo
+
+
+class TodoAddForm (forms.ModelForm):
+    class Meta:
+        model= Todo
+        fields=('title',)
+        # , tupple olduğu için konuldu yoksa hata verir
+        # exclude=('title',)  title hariç hepsi
+        
+
+        
+
+        
---
+++ src/todo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home, todo_list
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("list/", todo_list, name="todo_list")
+]
